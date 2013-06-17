c777eefae6997fe9b939fc051eef07aafd8baff0
Federico Farías Sánchez <cobrakik01@gmail.com>
2013-06-17 11:17:44 -0500

    Ejemplo 1
    Mi Primer Aplicación en C#

---
+++ EjemploPrimerApp/EjemploPrimerApp/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace EjemploPrimerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.Location = new System.Drawing.Point(33, 50);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(98, 31);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.Text = "Saludo";
+            this.lblSaludo.UseMnemonic = false;
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(12, 125);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(272, 23);
+            this.btnClick.TabIndex = 1;
+            this.btnClick.Text = "Haga Click Aqui";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 160);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.lblSaludo);
+            this.Name = "Form1";
+            this.Text = "Mi Primer Aplicacion C-Sharp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Button btnClick;
+    }
+}
+
